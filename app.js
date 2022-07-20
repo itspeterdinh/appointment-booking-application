d40e6e4fe76d4393261577696ddb0c485ee76144
Peter Dinh <peterdinh94@gmail.com>
2022-07-20 00:58:06 -0700

    update

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.enable('trust proxy');
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  //console.log(req.cookies);
+  next();
+});
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+module.exports = app;
